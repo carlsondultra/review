2a923d4ed77e35acf150d3c195322b82e6953354
carlsondultra <carlsondultra@users.noreply.github.com>
2023-06-11 20:50:54 -0400

    displaying hello world at localhost

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const express = require("express")
 
 const app = express()
 
+app.get("/", (req, res) => {
+    // console.log("hello world")
+    res.send("hello world")
+})
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
