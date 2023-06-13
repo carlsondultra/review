5f77c7f87925f116c3f5abcb4beeb0c6c5985e84
carlsondultra <carlsondultra@users.noreply.github.com>
2023-06-13 19:04:32 -0400

    adding delete and update routes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
 const express = require("express")
-
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("middleware testing")
-    next(); //tells middleware to go to next middleware or route handler
-})
+app.use(express.json())
 
 // Getting all the games
 app.get("/api/games", (req, res) => {
@@ -21,12 +17,47 @@ app.get("/api/games", (req, res) => {
 // Getting a single game 
 app.get("/api/games/:id", (req, res) => {
     console.log(req.params)
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            game: "testing"
+        }
+    })
 })
 
 // Creating a game
 app.post("/api/games", (req, res) => {
-    console.log(req)
+    console.log(req.body)
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            game: "testing"
+        }
+    })
 })
+
+// Updating a game
+app.put("/api/games/:id", (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    
+    res.status(200).json({
+        status: "success",
+        data: {
+            game: "testing"
+        }
+    })
+})
+
+// Deleting a game
+app.delete("/api/games/:id", (req, res) => {
+    res.status(204).json({
+        status: "success"
+    })
+})
+
 
 const port = process.env.PORT || 3001;
 
