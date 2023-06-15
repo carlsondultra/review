cca3580811dace4ed67173983893bbf8ebdb2e63
carlsondultra <carlsondultra@users.noreply.github.com>
2023-06-15 19:38:06 -0400

    finishing routes, adding try catch blocks and queries

--- server/server.js
+++ server/server.js
@@ -7,59 +7,85 @@ app.use(express.json())
 
 // Getting all the games
 app.get("/api/games", async (req, res) => {
-
-    const results = await db.query("select * from games")
-    console.log(results)
-    res.status(200).json({
-        status: "success",
-        data: {
-            game: ["game1", "game2"]
-        }
+    try{
+        const results = await db.query("select * from games")
+        console.log(results)
+        res.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                games: results.rows,
+            }
     })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 // Getting a single game 
-app.get("/api/games/:id", (req, res) => {
-    console.log(req.params)
-
-    res.status(200).json({
-        status: "success",
-        data: {
-            game: "testing"
-        }
-    })
+app.get("/api/games/:id", async (req, res) => {
+    console.log(req.params.id)
+    try {
+        // select * from games where id = req.params.id (used for avoiding sql injection)
+        const results = await db.query("select * from games where id = $1", [req.params.id])
+        res.status(200).json({
+            status: "success",
+            data: {
+                game: results.rows,
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 // Creating a game
-app.post("/api/games", (req, res) => {
+app.post("/api/games", async (req, res) => {
     console.log(req.body)
-
-    res.status(201).json({
-        status: "success",
-        data: {
-            game: "testing"
-        }
-    })
+    try {
+        const results = await db.query("INSERT INTO games (name, location, price_range) values($1, $2, $3) returning *", 
+        [req.body.name, req.body.location, req.body.price_range])
+        console.log(results)
+        res.status(201).json({
+            status: "success",
+            data: {
+                game: results.rows[0]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 // Updating a game
-app.put("/api/games/:id", (req, res) => {
+app.put("/api/games/:id", async (req, res) => {
+    try {
+        const results = await db.query("UPDATE games SET name = $1, location = $2, price_range = $3 where id = $4 returning *",
+        [req.body.name, req.body.location, req.body.price_range, req.params.id])
+        res.status(200).json({
+            status: "success",
+            data: {
+                game: results.rows[0],
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
     console.log(req.params.id)
     console.log(req.body)
-    
-    res.status(200).json({
-        status: "success",
-        data: {
-            game: "testing"
-        }
-    })
 })
 
 // Deleting a game
-app.delete("/api/games/:id", (req, res) => {
-    res.status(204).json({
-        status: "success"
-    })
+app.delete("/api/games/:id", async (req, res) => {
+    try {
+        const results = await db.query("DELETE FROM games where id = $1", [req.params.id])
+        res.status(204).json({
+            status: "success"
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 
