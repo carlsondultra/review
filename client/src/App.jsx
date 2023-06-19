ec246d3dad7ecf9143d6826a4945b586068f544d
carlsondultra <carlsondultra@users.noreply.github.com>
2023-06-18 20:10:03 -0400

    setting up routing via react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './routes/Home';
+import UpdatePage from './routes/UpdatePage';
+import GameDetailPage from './routes/GameDetailPage';
 
 const App = () => {
-    return <div>Testing</div>
+    return <div>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/games/:id/update" element={<UpdatePage/>}/>
+                <Route exact path="/games/:id" element={<GameDetailPage/>}/>
+            </Routes>
+        </Router>
+    </div>
 }
 
 export default App;
---
+++ client/src/routes/GameDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameDetailPage = () => {
+  return (
+    <div>Detail Page</div>
+  )
+}
+
+export default GameDetailPage
