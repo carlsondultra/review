dc88e7ef1f80514db40c0289d1291eb32244fcb5
carlsondultra <carlsondultra@users.noreply.github.com>
2023-06-19 19:59:42 -0400

    adding react context api and wrapping around app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,22 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './routes/Home';
 import UpdatePage from './routes/UpdatePage';
 import GameDetailPage from './routes/GameDetailPage';
+import { GamesContextProvider } from './context/GamesContext';
 
 const App = () => {
-    return <div class="container">
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home/>}/>
-                <Route exact path="/games/:id/update" element={<UpdatePage/>}/>
-                <Route exact path="/games/:id" element={<GameDetailPage/>}/>
-            </Routes>
-        </Router>
-    </div>
+    return (
+        <GamesContextProvider>
+            <div class="container">
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>}/>
+                        <Route exact path="/games/:id/update" element={<UpdatePage/>}/>
+                        <Route exact path="/games/:id" element={<GameDetailPage/>}/>
+                    </Routes>
+                </Router>
+            </div>
+        </GamesContextProvider>
+    )
 }
 
 export default App;
