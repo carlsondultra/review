f5a43b1e34956d83fd2e4b6b8f2de746a10a5b87
carlsondultra <carlsondultra@users.noreply.github.com>
2023-08-29 15:59:44 -0400

    updating title on page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
         <h1 className="font-weight-light display-1 text-center">
-            Game Finder
+            Game Reviews
         </h1>
     </div>
   )
